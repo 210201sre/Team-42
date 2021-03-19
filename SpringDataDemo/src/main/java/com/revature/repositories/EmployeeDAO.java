a50b0c10b7958fd1d0dec3bb51eb2f3162035a1c
Thomas Latza <thatguysquig@gmail.com>
2021-03-19 12:33:11 -0400

    fixed directory layout

--- SpringDataDemo/src/main/java/com/revature/repositories/EmployeeDAO.java
+++
@@ -1,7 +0,0 @@
-package com.revature.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.models.Employee;
-
-public interface EmployeeDAO extends JpaRepository<Employee, Integer> {}
