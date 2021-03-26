118f231968fdfbc93c11ab173cf43b25946750bc
Your Name <you@example.com>
2021-03-25 17:42:42 -0700

    created test for Acct services and added coverage

--- src/test/java/com/revature/services/AccountServicesTest.java
+++ src/test/java/com/revature/services/AccountServicesTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.hibernate.exception.JDBCConnectionException;
 //import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -101,6 +102,18 @@ public class AccountServicesTest {
 		assertEquals(123, cAccnt.getBalance());
 		assertFalse(cAccnt.getU().get(0).isEmployee());
 	}
+	
+	@Test
+	public void findAllCheckingsAccountsExceptionTest() {
+
+		
+		when(checkingsAccountDAO.findAll()).thenThrow(JDBCConnectionException.class);
+
+		// test
+		Set<CheckingsAccount> cAccnts = services.findAllCheckingsAccounts();
+		assertNull(cAccnts);
+	}
+
 
 	@Test
 	public void insertCheckingAccountTest() {
@@ -252,15 +265,12 @@ public class AccountServicesTest {
 		List<CheckingsAccount> Data2 = new ArrayList<CheckingsAccount>();
 		Data2.add(ca);
 		when(checkingsAccountDAO.findAll()).thenReturn(Data2);
-	//	when(checkingsAccountDAO.save(ca)).thenReturn(ca);
-		
+	
 		User user = new User();
 		user.setEmployee(false);
 		user.setPassword("password");
 		user.setUsername("user");
 		when(userDAO.findById(1)).thenReturn(Optional.of(user));
-		//when(userDAO.save(user)).thenReturn((user));
-		
 		
 		//test
 		assertNull(sa.getU());
